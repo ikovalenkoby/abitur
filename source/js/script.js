b6703d5260191c112d47ba947271dff7fb451b12
Iryna Kovalenko <ikovalenko@bsuir.by>
2021-04-28 18:16:29 +0300

    страница баллы прошлых лет усоверш. - оформлены стилями форма

--- source/js/script.js
+++ source/js/script.js
@@ -86,3 +86,51 @@ $(".toggler").click(function () {
   $(this).next().toggle("slow");
   $(this).closest('p.toggler').toggleClass("no-active");
 });
+
+//checkselect
+(function($) {
+  function setChecked(target) {
+    var checked = $(target).find("input[type='checkbox']:checked").length;
+    if (checked) {
+      $(target).find('select option:first').html('Выбрано: ' + checked);
+    } else {
+      $(target).find('select option:first').html('Факультеты');
+    }
+  }
+
+  $.fn.checkselect = function() {
+    this.wrapInner('<div class="checkselect-popup"></div>');
+    this.prepend(
+      '<div class="checkselect-control">' +
+      '<select class="form-control"><option></option></select>' +
+      '<div class="checkselect-over"></div>' +
+      '</div>'
+    );
+
+    this.each(function(){
+      setChecked(this);
+    });
+    this.find('input[type="checkbox"]').click(function(){
+      setChecked($(this).parents('.checkselect'));
+    });
+
+  /*  this.parent().find('.checkselect-control').on('click', function(){
+      $popup = $(this).next();
+      $('.checkselect-popup').not($popup).css('display', 'none');
+      if ($popup.is(':hidden')) {
+        $popup.css('display', 'block');
+        $(this).find('select').focus();
+      } else {
+        $popup.css('display', 'none');
+      }
+    });
+
+    $('html, body').on('click', function(e){
+      if ($(e.target).closest('.checkselect').length == 0){
+        $('.checkselect-popup').css('display', 'none');
+      }
+    });*/
+  };
+})(jQuery);
+$('.checkselect').checkselect();
+
